e5e25c1f9ce10ce5f2dbc709f8be4b3634676aaf
jacob-techhub <82525832+jacob-techhub@users.noreply.github.com>
2021-06-18 09:59:22 -0500

    Score UI finished
    Time, score, customer total, customer serving are all finished.

--- Assets/ScoringUI.cs
+++ Assets/ScoringUI.cs
@@ -6,16 +6,20 @@ using UnityEngine.UI;
 public class ScoringUI : MonoBehaviour
 {
     // Counters in UI, set them by assigning a String (or int converted to String) to the .text variable
+    public Text customersTotalText;
     public Text customerNumText;
     public Text avgTimeText;
     public Text currentTimeText;
+    public Text scoreText;
 
     // Customer manager
     public CustomerOrder inv;
 
     // variables to store
+    public int totalCustomers = 0;
     public int currentCustomer = 0;
     public int avgTime = 0; // how do we keep this accurate? Tricky problem here. 
+    public int score = 0;
 
 
     // Start is called before the first frame update
@@ -25,7 +29,10 @@ public class ScoringUI : MonoBehaviour
         customerNumText.text = "" + currentCustomer;
         avgTimeText.text = "" + avgTime;
         currentTimeText.text = "" + 0;
+        scoreText.text = "" + 0;
 
+        // Set the total number of customers at the beginning of the level
+        //customersTotalText.text = "" + totalCustomers;
     }
 
     private void Update()
@@ -34,6 +41,9 @@ public class ScoringUI : MonoBehaviour
         avgTimeText.text = "" + avgTime;
         //currentTimeText.text = "" + 0;
         currentTimeText.text = "" + (int)inv.timeSinceOrderTaken;
+
+        // This is put in update because I know it will work here, but I'm not 100% sure it would work in Start()
+        customersTotalText.text = "" + totalCustomers;
     }
 
     // Call every frame in Update method
@@ -45,7 +55,21 @@ public class ScoringUI : MonoBehaviour
         // convert to int
 
         // add to average (a little tricky, normalize over the proportion it will contribute
+    }
 
+    public void addScoreToUI()
+    {
+        Debug.Log("ADDING SCOREEEE");
+        if (inv.timeSinceOrderTaken <= 40)
+        {
+            score += 100;
+            scoreText.text = "" + score;
+        }
+        else
+        {
+            score += 75;
+            scoreText.text = "" + score;
+        }
     }
 
 }
